a7c278e047c7d64b3a6a976dea351e1d00e9e8da
sirawitrakkaew <138280187+sirawit1rakkaew@users.noreply.github.com>
2024-07-10 11:05:25 +0700

    Add files via upload

---
+++ HW02_6710301018 - Copy.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Job Code: HW02
+Job Desc: The biggest of Three
+STUD ID: 6710301018
+STUD Name: Sirawit Rakkaew
+Date: 2024-06-25
+"""
+a=input("get A :")
+b=input("get B :")
+
+if a<b:
+    big=b
+    small=a
+else:
+    big=a
+    small=b
+c=small
+Sum=0
+for c in range(big):
+    Sum=Sum+c
+    c=c+1
+print(f'{a} {b} {Sum}')
